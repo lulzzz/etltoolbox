2716b8ff563ed2b9e1b6bd604639cb9dfdb89b2e
roadrunnerlenny <andreaslennartz@gmail.com>
2018-07-23 21:30:32 +0200

    Creating new DF

---
+++ ETLToolbox/Toolbox/DataFlow/NewDataFlowTask.cs
@@ -0,0 +1,62 @@
+﻿using ALE.ETLToolbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ALE.ETLToolbox
+{
+    public class NewDataFlowTask<TSource,TTarget> : GenericTask, ITask 
+        where TSource : class,new() 
+        where TTarget : class,new()
+    {
+        /* ITask Interface */
+        public override string TaskType { get; set; } = "NEWDATAFLOW";
+        public override string TaskName { get; set; }
+
+        /* Public properties */
+   
+        NLog.Logger NLogger { get; set; }
+
+        public Table InputTable { get; set; }
+        public Table OutputTable { get; set; }
+
+        public Func<TSource,TTarget> RowTrans { get; set; }
+
+        public NewDataFlowTask()
+        {
+            NLogger = NLog.LogManager.GetLogger("Default");
+        }
+
+        public NewDataFlowTask(string name, Table inputTable, Table outputTable) : this()
+        {
+            TaskName = name;
+            InputTable = inputTable;
+            OutputTable = outputTable;
+
+        }
+
+
+        public override void Execute()
+        {
+            TransformBlock<TSource, TTarget> tb = new TransformBlock<TSource, TTarget>(input =>
+            {
+                //Do something here with inputTable?
+                return RowTrans(input); 
+            }
+                );
+
+        }
+            
+    
+
+        //public static void Execute(string name, Table inputTable, Table outputTable) => new NewDataFlowTask(name, inputTable, outputTable).Execute();  
+    }
+
+    public class Table : List<TableColumn>
+    {
+        
+    }
+}
---
+++ ETLToolboxTest/DataFlow/TestNewDataFlow.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ALE.ETLToolbox;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ALE.ETLToolboxTest {
+    [TestClass]
+    public class TestNewDataFlow {
+        public TestContext TestContext { get; set; }
+        public string ConnectionStringParameter => TestContext?.Properties["connectionString"].ToString();
+        public string DBNameParameter => TestContext?.Properties["dbName"].ToString();
+
+        [ClassInitialize]
+        public static void TestInit(TestContext testContext) {
+            TestHelper.RecreateDatabase(testContext);
+            ControlFlow.CurrentDbConnection = new SqlConnectionManager(new ConnectionString(testContext.Properties["connectionString"].ToString()));
+            CreateSchemaTask.Create("test");            
+        }
+
+        TableColumn keyCol => new TableColumn("Key", "int", allowNulls: false, isPrimaryKey: true) { IsIdentity = true };
+        TableColumn col1 => new TableColumn("ValueAsString", "nvarchar(100)", allowNulls: false);
+        TableColumn col2 => new TableColumn("ValueAsInt", "int", allowNulls: true);
+
+        [TestMethod]
+        public void TestDB2DBDataflow() {
+            //TODO Table Objekt anlegen der Vollständigkeit halber
+            Table inputT = new Table();
+            inputT.Add(keyCol);
+            inputT.Add(col1);
+            Table outputT = new Table();
+            outputT.Add(keyCol);
+            outputT.Add(col2);
+
+
+            CreateTableTask.Create("test.Source", new List<TableColumn>() {keyCol, col1});
+            CreateTableTask.Create("test.Destination", new List<TableColumn>() { keyCol, col2});
+
+            NewDataFlowTask<InputDS, OutputDS> df = new NewDataFlowTask<InputDS, OutputDS>();
+            df.RowTrans = input => new OutputDS();
+            
+
+            //DataFlowTask.Execute("Test dataflow task", "DataFlow/InputData.csv", "test.Staging", 3, RowTransformation, BatchTransformation);
+            //Assert.AreEqual(4, SqlTask.ExecuteScalar<int>("Check staging table", "select count(*) from test.Staging"));                        
+        }
+
+        public string[] RowTransformation(string[] row) {
+            return row;
+        }
+
+        //public InMemoryTable BatchTransformation(string[][] batch) {
+        //    InMemoryTable table = new InMemoryTable();
+        //    table.HasIdentityColumn = true;
+        //    table.Columns.Add(new InMemoryColumn(col1));
+        //    table.Columns.Add(new InMemoryColumn(col2));
+        //    table.Columns.Add(new InMemoryColumn(col3));
+
+        //    foreach (string[] row in batch)
+        //        table.Rows.Add(row);
+        //    return table;
+        //}
+    }
+
+    public class InputDS
+    {
+        public int Key { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class OutputDS
+    {
+        public int Key { get; set; }
+        public int Value { get; set; }
+    }
+
+}
